fdbabc91eafcefb42df9c61b7c5910d1bdc13c4d
Uwish <skemiil@atoms.rocks>
2022-06-18 19:58:34 -0500

    First commit

---
+++ flooder.py
@@ -0,0 +1,18 @@
+import requests
+import os
+import random
+import string
+import json
+# -------------------------------------------
+# ngl.link flooder by SkemiiL
+# -------------------------------------------
+text = input("Text: ")
+url = input("url: ")
+amt = input("Amount of messages: ")
+amt = int(amt)
+
+for x in range(0, amt):
+    requests.post(url, allow_redirects=False, data={
+    "question": text
+    })
+    print("Sending text: %s to %s. Message %i out of %i" % (text, url, x, amt))
