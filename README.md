659e76628f36c5ce021103aaf7f77a575aabe833
Uwish <skemiil@atoms.rocks>
2022-06-19 17:13:09 -0500

    Added lots of changes to make it work better

---
+++ README.md
@@ -0,0 +1,15 @@
+# ngl.link Flooder
+This is a flooder for ngl.link since I noticed its been getting traction on instagram and snapchat
+
+## Preview
+![fullscreen](https://github.com/Skemiil/ngl.link-flooder/blob/master/preview.png?raw=true)
+
+## Usage
+1. Download
+2. Run "python flooder.py"
+3. Fill out the three options
+4. Your done
+
+## Requirements
+1. Python3
+2. Requests
--- flooder.py
+++ flooder.py
@@ -2,15 +2,27 @@ import requests
 # -------------------------------------------
 # ngl.link flooder by SkemiiL
 # -------------------------------------------
-text = input("Text: ")
-url = input("url: ")
-amt = input("Amount of messages: ")
-amt = int(amt)
 
+# defining logo and printing it
+logo = """
+             _   _ _       _       __ _                 _           
+ _ __   __ _| | | (_)_ __ | | __  / _| | ___   ___   __| | ___ _ __ 
+| '_ \ / _` | | | | | '_ \| |/ / | |_| |/ _ \ / _ \ / _` |/ _ \ '__|
+| | | | (_| | |_| | | | | |   <  |  _| | (_) | (_) | (_| |  __/ |   
+|_| |_|\__, |_(_)_|_|_| |_|_|\_\ |_| |_|\___/ \___/ \__,_|\___|_|   
+       |___/                                         by Skemiil
+"""
+print(logo)
+
+# user input
+text = input("Text: ")
+url = "https://ngl.link/" + input("username: ")
+amt = int(input("Amount of messages: "))
+
+# sending a post request to ngl.link
 for x in range(0, amt):
     requests.post(url, allow_redirects=False, data={
     "question": text
     })
-    print("Sending text: %s to %s. Message %i out of %i" % (text, url, x, amt))
-
-print("Finished flood!")
+    print("[+] Sending text: %s to %s. Message %i out of %i" % (text, url, x, amt))
+print("[-] Finished flood!")
